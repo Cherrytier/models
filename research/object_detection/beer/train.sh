fa2bb1ed31b80f7ee8c58d00fa2f878a1bc92440
grandesty <gangkanli1219@163.com>
2017-12-06 09:36:03 +0800

    fix

--- research/object_detection/beer/train.sh
+++ research/object_detection/beer/train.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 export PYTHONPATH=$PYTHONPATH:$DIR/../../
 export PYTHONPATH=$PYTHONPATH:$DIR/../../slim
+
 TRAIN=/home/admins/cmake/ssd_mobilenet_v1_coco_11_06_2017
+
 python3 $DIR/../train.py \
 --train_dir $TRAIN \
 --pipeline_config_path $TRAIN/ssd_mobilenet_beer.config
